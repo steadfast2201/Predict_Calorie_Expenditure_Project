2e6f71afd943bfc4efd1fe066840af5952604af4
steadfast2201 <patilaniket953765@gmail.com>
2025-05-26 14:56:20 +0530

    First Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='I am working on making a Project to predict calorie expenditure for a normal human being using provided data from a Kaggle Competition',
+    author='Aniket_Patil',
+    license='MIT',
+)
